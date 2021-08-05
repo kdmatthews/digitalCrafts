1a771c4b2aec34b8a7ed6a23fc0269212b8dc6ed
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-05 17:13:23 -0400

    update todo running

--- week1/day4/todo.py
+++ week1/day4/todo.py
@@ -1,6 +1,8 @@
 menu=True 
 todo = {'Wash the car': 'low', 'Mow the lawn': 'medium', 'Walk the dog': 'high', 'Do the dishes': 'high'}
-while menu < 5:
+task_list = ['wash the dog', 'mow the lawn', 'take out the trash']
+
+while menu:
     print ("""
     Press 1 to add a task
     Press 2 to delete a task
@@ -10,11 +12,13 @@ while menu < 5:
     break
 menu=input("What would you like to do?\n")
 
-task_list = ['Wash the car', 'Mow the lawn', 'Walk the dog', 'Do the dishes']
+
 if menu == '1':
     title_input = input("Please input the title of the task.\n")
     priority_input = input("Please input the priority as 'high', 'medium', or 'low'.\n")
     task_list.append(title_input)
+    print("Here is your to do list: ")
+    print(task_list)
 elif menu == '2':
     for i, element in enumerate(task_list):
         print(i, element)
@@ -23,8 +27,11 @@ elif menu == '2':
     print("Here is your new to do list: ")
     print(task_list)
 elif menu == '3':
-    for title, priority in todo.items():
-        print(title, priority)
+    for i, todo in enumerate(todo.items()):
+        i += 1
+        print(i, todo)
+    # for title, priority in sorted(todo.items()):
+    #     print(title, priority)
 elif menu == 'q':
     print("Have a nice day!")
     SystemExit
