e42f68c0cc3628ebe12993be9411fb2953be5ff2
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-05 17:25:56 -0400

    todo fixed menu loop

--- week1/day4/todo.py
+++ week1/day4/todo.py
@@ -9,33 +9,30 @@ while menu:
     Press 3 to view all tasks
     Press q to quit
     """)
-    break
-menu=input("What would you like to do?\n")
+    menu=input("What would you like to do?\n")
+    if menu == '1':
+        title_input = input("Please input the title of the task.\n")
+        priority_input = input("Please input the priority as 'high', 'medium', or 'low'.\n")
+        task_list.append(title_input)
+        print("Here is your to do list: ")
+        print(task_list)
+    elif menu == '2':
+        for i, element in enumerate(task_list):
+            print(i, element)
+        delete_task = int(input("Type the number of the task you would like to delete: "))
+        del task_list[delete_task]
+        print("Here is your new to do list: ")
+        print(task_list)
+    elif menu == '3':
+        for i, todo in enumerate(todo.items()):
+            i += 1
+            print(i, todo)
+        # for title, priority in sorted(todo.items()):
+        #     print(title, priority)
+    elif menu == 'q':
+        print("Have a nice day!")
+        break
+    else:
+        print("Error! Be sure to type '1', '2', '3', or 'q'")
 
-
-if menu == '1':
-    title_input = input("Please input the title of the task.\n")
-    priority_input = input("Please input the priority as 'high', 'medium', or 'low'.\n")
-    task_list.append(title_input)
-    print("Here is your to do list: ")
-    print(task_list)
-elif menu == '2':
-    for i, element in enumerate(task_list):
-        print(i, element)
-    delete_task = int(input("Type the number of the task you would like to delete: "))
-    del task_list[delete_task]
-    print("Here is your new to do list: ")
-    print(task_list)
-elif menu == '3':
-    for i, todo in enumerate(todo.items()):
-        i += 1
-        print(i, todo)
-    # for title, priority in sorted(todo.items()):
-    #     print(title, priority)
-elif menu == 'q':
-    print("Have a nice day!")
-    SystemExit
-else:
-    print("Error! Be sure to type '1', '2', '3', or 'q'")
-
-        
+            
