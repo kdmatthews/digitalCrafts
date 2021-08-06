d5269a62fd40acc1447f7c45ac649198121430de
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-06 15:37:01 -0400

    Update todo2.py

--- week1/day4/todo2.py
+++ week1/day4/todo2.py
@@ -1,4 +1,3 @@
-# To Do List
 todo = {}
 task_list = []
 while True:
@@ -12,17 +11,13 @@ while True:
     if menu == '1':
         title_input = input("Please input the title of the task.\n")
         priority_input = input("Please input the priority as 'high', 'medium', or 'low'.\n")
-        # print("Here is your to do list: ")
         todo = {title_input: priority_input}
         task_list.append(todo)
-        # print(task_list)
     elif menu == '2':
         for i, element in enumerate(task_list):
             print(i, element)
         delete_task = int(input("Type the number of the task you would like to delete: "))
         del task_list[delete_task]
-        # print("Here is your new to do list: ")
-        # print(task_list)
     elif menu == '3':
         for i, element in enumerate(task_list):
             i += 1
