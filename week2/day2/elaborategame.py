6a0222348b007437aeac0a3f09bc4950a426a76a
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-13 12:32:54 -0400

    Added Quit Feature

--- week2/day2/elaborategame.py
+++ week2/day2/elaborategame.py
@@ -19,10 +19,15 @@ class Characters():
     def attack_player(self):
         self.attack -= 10
     
+    def player_shielded(self):
+        self.attack -= 5
+    
+    def attack_while_eating(self):
+        self.health -= 5
         
 
-grogu = Characters('Grogu', 50, 50)
-mudhorn = Characters('Mudhorn', 50, 50)
+grogu = Characters('Grogu', 50, 100)
+mudhorn = Characters('Mudhorn', 50, 100)
 mando = Characters('Mando', 100, 100)
 moffGideon = Characters('Moff Gideon', 100, 100)
 
@@ -95,23 +100,37 @@ def main_mando():
         menu_mando()
         user_choice = input("Please select your move!: ")
         if user_choice == "1":
-            random_number = random.randrange(11)
-            if random_number < 5:
-                moffGideon.take_damage()
-                mando.attack_player()
-                print("You have used your Beskar Spear against Moff Gideon! He has taken 10 damage.")
+            if mando.attack < 10:
+                print("You do not have enough attack points to attack Moff Gideon.")
             else:
-                mando.attack_player()
-                print("Moff Gideon dodged your attack.")
+                random_number = random.randrange(11)
+                if random_number < 7:
+                    moffGideon.take_damage()
+                    mando.attack_player()
+                    print("You have used your Beskar Spear against Moff Gideon! He has taken 10 damage.")
+                else:
+                    mando.attack_player()
+                    print("Moff Gideon dodged your attack.")
         elif user_choice == "2":
-            if mando.health < 100:
+            random_number = random.randrange(11)
+            if mando.health > 99:
+                print("You are at maximum health!")
+            elif random_number < 7:
                 mando.eat()
                 print("You have eaten bone broth. 10 has been added to your health!")
             else:
-                print("You are at maximum health")
+                mando.eat()
+                mando.attack_while_eating()
+                print("Moff Gideon got the jump on you while you were slurping your soup. Only 5 health points were gained.")
         elif user_choice == '3':
-            mando.shield_failure()
-            print("You were not quick enough. Moff Gideon hit you with the Darksaber.You take 10 damage ")
+            random_number = random.randrange(11)
+            if random_number < 5:
+                mando.shield_failure()
+                moffGideon.attack_player()
+                print("You were not quick enough. Moff Gideon hit you with the Darksaber.You take 10 damage ")
+            else:
+                moffGideon.player_shielded()
+                print("You were able to move out of the way. Moff Gideon struck a wall and lost 5 attack points.")
         elif user_choice == '4':
             print(mando.health)  
             print(mando.attack)
