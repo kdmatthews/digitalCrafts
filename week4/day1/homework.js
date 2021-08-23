dee42495fd4708fc69e7fe0621a26a0f1b2d1523
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-23 17:19:41 -0400

    homework through long vowels

--- week4/day1/homework.js
+++ week4/day1/homework.js
@@ -64,11 +64,80 @@ function printSquare(size){
        console.log("---")
     }
 }
-console.log(printSquare(5));
+// (printSquare(5));
 
 // Print Box
+// This prints the right number but lines them up instead of placing them in a square.
 function printBox(width, height){
-    let box = Math.pow(width, height);
-    return box
+    for(let index = 1; index <= width; index++){
+        console.log("-");
+    } for(let h = 1; h <= height; h++){
+        console.log('|');
+    } 
 }
-console.log(printBox(3, 4));
+(printBox(3, 4));
+// This does not print the correct numbers or in box shape
+// function printBox(width, height){
+//     box = ""
+//     for(let w = 1; w <= width; w++){
+//         for(let h = 1; h <= height; h++){
+//             if(w === 0 || w === width){
+//                 box += "-";
+//             } else if (h === 0 || h === height){
+//                 box += "|";
+//             } else {
+//                box += "";
+//             }
+//         } box += "\n";
+//     }
+        
+
+
+
+// Print a Banner
+function printBanner(text){
+    let dash = ""
+    for (item in text){
+        dash += "-"
+        
+    }
+    console.log(dash)
+    console.log(text)
+    console.log(dash)
+}
+    // for(let index = 0; index <= text.length; index++){
+        
+    // }
+printBanner('Happy Birthday')
+
+// letspeak
+// This will replace it the first time the letter pops up but not the next.
+function leetspeak(string){
+    let str = string
+    str = str.replace("A", "4");
+    str = str.replace("E", "3");
+    str = str.replace("G", "6");
+    str = str.replace("I", "1");
+    str = str.replace("O", "0");
+    str = str.replace("S", "5");
+    str = str.replace("T", "7");
+    console.log(str)
+                
+    }
+
+
+leetspeak("LEETSPEAK")
+
+// Long-long Vowels
+function longVowels(string){
+    str = ""
+    for (let vowel = 0; vowel < string.length; vowel++){
+        if (string[vowel] === 'a' || string[vowel] === 'e' || string[vowel] === 'i' || string[vowel] === 'o' || string[vowel] === "u"){
+            str += string[vowel].repeat(5);
+        } else {
+            str += string[vowel]
+        }
+    } return str
+}
+
+console.log(longVowels("Good Cheese"))
