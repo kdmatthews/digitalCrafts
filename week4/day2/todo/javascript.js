270b98e88bd48f16d3a35ddb020eea1640815ec5
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-24 16:36:48 -0400

    remove all button added

--- week4/day2/todo/javascript.js
+++ week4/day2/todo/javascript.js
@@ -1,15 +1,21 @@
 const container = document.querySelector('.main-container');
 const input = document.querySelector('.todo-items');
 const submit = document.querySelector('.submit-button');
+const remove = document.querySelector('.removeall');
+const second = document.querySelector('.second');
+
 
 const header = document.createElement('h1');
 // const directions = document.createElement('h3');
 const list = document.createElement('p');
 
-header.innerHTML = 'To-Do List';
+
+header.innerHTML = 'To Do List';
 // directions.innerHTML = 'Please enter the items you would like to add to your list.'
 
 container.append(header);
+
+
 
 
 function inputItems(){
@@ -18,11 +24,21 @@ function inputItems(){
     const item = document.createElement("li")
     item.append(valueOfInput)
     container.append(item)
-    
-    
+   
+  
 }
-function createArray (){
-    const array = []
-    array.append(valueOfInput)
+function removeAll(){
+    document.querySelector('.main-container').innerHTML="";
+    container.append(header);
 }
-submit.onclick = inputItems;
+
+function resetInput(){
+    document.querySelector(".todo-items").value=null;
+    
+};
+submit.addEventListener("click", () => {
+    inputItems();
+    document.querySelector(".todo-items").value=null;
+});
+// submit.onclick = inputItems;
+remove.onclick = removeAll;
