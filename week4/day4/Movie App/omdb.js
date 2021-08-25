69b823ef58a414f83275f4d3181fce39faf08063
Kayla Matthews <kdmatthews77@gmail.com>
2021-08-25 16:55:51 -0400

    movie app in progress -undefined

--- week4/day4/Movie App/omdb.js
+++ week4/day4/Movie App/omdb.js
@@ -1,2 +1,28 @@
-const omdburl = "http://www.omdbapi.com/?i=tt3896198&apikey=c3ed5183";
+const apiKey = "c3ed5183";
+const omdburl = `https://www.omdbapi.com/?apikey=${apiKey}`;
 
+const movieContainer = document.querySelector('.main-container')
+const searchButton = document.querySelector('.search-button')
+
+async function movieSearch(){
+    const input = document.querySelector(".input").value;
+    const fetchMovies = await fetch(omdburl + `&s=${input}`);
+    const jsonMovies = await fetchMovies.json();
+    console.log("hello");
+
+    for (const movie in jsonMovies.Search){
+        // const movieDiv = document.createElement("div")
+        const titleMovie = document.createElement('h2');
+        titleMovie.innerText = movie.Title;
+        
+        const posterMovie = document.createElement('img');
+        posterMovie.src = movie.Poster;
+
+        const yearMovie = document.createElement('h4');
+        yearMovie.innerText = movie.Year;
+
+        movieContainer.append(titleMovie, posterMovie, yearMovie)
+    }
+}
+
+searchButton.addEventListener("click", () => movieSearch());
