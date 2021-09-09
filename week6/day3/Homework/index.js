6f32580ea92956508a6e1ff21d27f188710a942e
Kayla Matthews <kdmatthews77@gmail.com>
2021-09-09 16:10:36 -0400

    mostly working except updating multiple rows

--- week6/day3/Homework/index.js
+++ week6/day3/Homework/index.js
@@ -12,9 +12,45 @@ app.use(express.json());
 
 app.post('/createStock', async (req, res) => {
     const { data, error } = await 
-    supabase.from('Stocks').insert([req.body])
+    supabase.from('Stocks').insert(req.body)
     res.send("It went through")
+}) 
+
+app.delete('/deleteStock/:stockId', async (req, res) => {
+    const id = req.params.stockId
+    const { data, error } = await 
+    supabase.from('Stocks').delete().match({ id:id });
+    res.send(data)
+})
+app.get('/readStock', async (req, res) => {
+    const { data, error } = await supabase
+    .from('Stocks')
+    .select()
+    res.send(data)
 })
 
+app.put('/updateStock/:stockId', async (req, res) => {
+    const id = req.params.stockId
+    const { data, error } = await supabase
+    .from('Stocks')
+    .update(req.body)
+    .match({id:id});
+    res.send(data);
+})
+
+app.get('/readStock/:stockId', async (req, res) => {
+    const id = req.params.stockId
+    const { data, error } = await supabase
+    .from('Stocks')
+    .select().match({ id:id });
+    res.send(data)
+})
+app.get('/stockCriteria/:stockCriteria', async (req, res) => {
+    const criteria = req.params.stockCriteria
+    const { data, error } = await supabase
+    .from('Stocks')
+    .select(criteria)
+    res.send(data)
+})
 
 app.listen(PORT, console.log(`Listening at port ${PORT}`))
