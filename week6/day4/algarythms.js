580d71db1b1f8f7b4443a9ff005cbcf7f52530b0
Kayla Matthews <kdmatthews77@gmail.com>
2021-09-10 10:48:43 -0400

    second function complete

--- week6/day4/algarythms.js
+++ week6/day4/algarythms.js
@@ -1,13 +1,34 @@
 
 
-function formatDate(){
-    let str = '09/10/2021';
+function formatDate(str){
+    // let str = '09/10/2021';
     for (const char in str){
         str = str.replace('/', "")
         
     }
     str = str.slice(-4) + str.slice(0, -4)
     console.log(str);
+    return str;
 }
 
-formatDate()
+formatDate('09/10/2021');
+
+function checkString(str){
+    const strSplit = str.split("");
+    const strReverse = strSplit.reverse();
+    const strJoin = strReverse.join("");
+    console.log(strJoin)
+    if (strJoin === str){
+        console.log(true)
+        return true
+    }
+    else{
+        console.log(false)
+        return false
+    }
+}
+
+checkString('kayak');
+checkString('backendisawesome');
+checkString('mom')
+
