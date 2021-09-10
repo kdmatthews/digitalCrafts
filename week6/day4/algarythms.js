bff477df4724908cf58c9a7460000e8808e21c4a
Kayla Matthews <kdmatthews77@gmail.com>
2021-09-10 14:50:08 -0400

    stocks complete

--- week6/day4/algarythms.js
+++ week6/day4/algarythms.js
@@ -14,10 +14,13 @@ function formatDate(str){
 formatDate('09/10/2021');
 
 function checkString(str){
+    for (char in str){
+        const strFix = str.replace(" ", "");
+    }
+  
     const strSplit = str.split("");
     const strReverse = strSplit.reverse();
     const strJoin = strReverse.join("");
-    console.log(strJoin)
     if (strJoin === str){
         console.log(true)
         return true
@@ -28,7 +31,22 @@ function checkString(str){
     }
 }
 
-checkString('kayak');
-checkString('backendisawesome');
+checkString('ka yak');
+checkString('backendis awesome');
 checkString('mom')
+checkString('race car')
 
+function minMaxSum(arr){
+       for (number in arr){
+         console.log(Math.min(number))
+       }
+    }
+// minMaxSum([1, 2, 3, 4, 5]);
+
+function anagrams(array){
+    for (const word in array){
+        console.log(word)
+    }
+}
+
+anagrams(['yo', 'act', 'flop', 'olfp', 'tac', 'oy'])
