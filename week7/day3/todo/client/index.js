d6f80dc6cc931750329271366f3d8d3c11566700
Kayla Matthews <kdmatthews77@gmail.com>
2021-09-15 17:08:50 -0400

    reading data

--- week7/day3/todo/client/index.js
+++ week7/day3/todo/client/index.js
@@ -8,6 +8,37 @@
 //     sendTodo(e);
 //
 
+const mainContainer = document.querySelector(".main-container")
+
+
+const readData = async () => {
+    const url = "http://localhost:3007/getItems";
+    const userData = await fetch (url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            'Content-Type': 'application/json',
+            },
+        
+    });
+    const json = await userData.json();
+    for(const item of json) {
+        const todo_item = item.todo_item
+        const itemName = document.createElement('p');
+        const itemDetails = document.createElement('div')
+        itemName.innerHTML = todo_item
+        itemDetails.append(itemName);
+        mainContainer.append(itemDetails);
+        
+
+    }
+    console.log(json)
+
+}
+const readButton = document.querySelector(".read")
+readButton.addEventListener("click", () => {
+    readData()
+})
 const submitButton = document.querySelector(".submit")
 const createItem = async () => {
     const url = "http://localhost:3007/createItem"
@@ -31,7 +62,7 @@ const createItem = async () => {
 submitButton.addEventListener("click", () => {
     createItem();
 })
-// const mainContainer = document.querySelector(".main-container")
+
 
 // async function getItems() {
 //     const fetchItems = await fetch("http://localhost:3007/getItems", {
