367177a5e3b964030b96662a781cbc0ebcc13270
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-04 10:39:18 -0400

    paths added to routes

--- week9/day4/form-validations-2/src/App.js
+++ week9/day4/form-validations-2/src/App.js
@@ -2,17 +2,27 @@ import Form from "./components/Form";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import { MainDiv } from "./styled-components/FormStyle";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 function App() {
   return (
-    (<div className="App">
-      
-     
+  <Router>
       <Header />
       <MainDiv>
       <Sidebar />
-      <Form />
+      <Route path="/form">
+        <Form />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
       </MainDiv>
-    </div>)
+    </Router>
   );
 }
 
