10a9559d99d13d2f689544272b9fc3c22afcd33e
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-07 12:21:54 -0400

    debounce functioning

--- week10/day4/redux-day3/src/components/SignUpForm.js
+++ week10/day4/redux-day3/src/components/SignUpForm.js
@@ -1,18 +1,22 @@
-import React, { useState } from 'react';
+import React, { useMemo } from 'react';
 import "./SignUpStyle.css";
 import { useDispatch } from 'react-redux';
+import debounce from "lodash.debounce";
 
 
 export default function SignUpForm() {
    
     const dispatch = useDispatch()
+    const debouncedChangeHandler = useMemo(() => {
+        return debounce(dispatch, 3000);
+    }, []);
     return (
         <div className="signupform">
             <form action="">
-            <input onChange={(e) => dispatch({ type: "SIGNUPFORM_FIRSTNAME", payload:e.target.value})}type="text" placeholder="First Name"/>
-            <input type="text" placeholder="Last Name" />
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Password"/>
+            <input onChange={(e) =>debouncedChangeHandler({ type: "SIGNUPFORM_FIRSTNAME", payload:e.target.value, })}type="text" placeholder="First Name"/>
+            <input onChange={(e) =>debouncedChangeHandler({ type: "SIGNUPFORM_LASTNAME", payload:e.target.value, })}type="text" placeholder="Last Name" />
+            <input onChange={(e) =>debouncedChangeHandler({ type: "SIGNUPFORM_EMAIL", payload:e.target.value, })}type="email" placeholder="Email"/>
+            <input onChange={(e) =>debouncedChangeHandler({ type: "SIGNUPFORM_PASSWORD", payload:e.target.value, })}type="password" placeholder="Password"/>
             <input type="submit"  />
             </form>
         </div>
