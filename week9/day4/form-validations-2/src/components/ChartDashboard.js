bb4aae5fe86c298038cc1d294770541ad7d0cf28
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-11 16:10:01 -0400

    graph data moved into the reducer

--- week9/day4/form-validations-2/src/components/ChartDashboard.js
+++ week9/day4/form-validations-2/src/components/ChartDashboard.js
@@ -1,38 +1,42 @@
 import React from 'react'
 import { ChartDiv } from '../styled-components/Dashboard';
-import {Pie, Doughnut} from 'react-chartjs-2';
+import {Pie} from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
 
-const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      backgroundColor: [
-        '#4297A0',
-        '#F9BDC0',
-        '#B5DFD6',
-        '#F8EA8C',
-        '#9F5060'
-      ],
-      hoverBackgroundColor: [
-      '#2F5061',
-      '#ED7890',
-      '#91CABE',
-      '#E1C340',
-      '#AC203D'
-      ],
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
-}
 
-export default class ChartDashboard extends React.Component {
-  render() {
+// const state = {
+//   labels: ['January', 'February', 'March',
+//            'April', 'May'],
+//   datasets: [
+//     {
+//       label: 'Rainfall',
+//       backgroundColor: [
+//         '#4297A0',
+//         '#F9BDC0',
+//         '#B5DFD6',
+//         '#F8EA8C',
+//         '#9F5060'
+//       ],
+//       hoverBackgroundColor: [
+//       '#2F5061',
+//       '#ED7890',
+//       '#91CABE',
+//       '#E1C340',
+//       '#AC203D'
+//       ],
+//       data: [65, 59, 80, 81, 56]
+//     }
+//   ]
+// }
+// const state = useSelector((state) => state.GraphData)
+// export default class ChartDashboard extends React.Component 
+export default function ChartDashboard(){
+const data = useSelector((state) => state.GraphData)
+console.log(data)
     return (
       <ChartDiv>
         <Pie
-          data={state}
+          data={data}
           options={{
             title:{
               display:true,
@@ -50,7 +54,8 @@ export default class ChartDashboard extends React.Component {
       </ChartDiv>
     );
   }
-}
+
+
 
 // export default function ChartDashboard() {
 //     return (
--- week9/day4/form-validations-2/src/reducers/graphData.js
+++ week9/day4/form-validations-2/src/reducers/graphData.js
@@ -1,6 +1,27 @@
 const initialState = {
-
-}
+    labels: ['January', 'February', 'March',
+             'April', 'May'],
+    datasets: [
+      {
+        label: 'Rainfall',
+        backgroundColor: [
+          '#4297A0',
+          '#F9BDC0',
+          '#B5DFD6',
+          '#F8EA8C',
+          '#9F5060'
+        ],
+        hoverBackgroundColor: [
+        '#2F5061',
+        '#ED7890',
+        '#91CABE',
+        '#E1C340',
+        '#AC203D'
+        ],
+        data: [65, 59, 80, 81, 56]
+      }
+    ]
+  }
 
 const GraphData = (state=initialState, action) => {
     switch(action.type){
