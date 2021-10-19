b8e7ba6d5c1f65a4929da100fae84e30970948f2
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-19 12:10:25 -0400

    counter created

--- week12/day1/react-review/src/App.js
+++ week12/day1/react-review/src/App.js
@@ -6,8 +6,11 @@ import {useDispatch, useSelector} from "react-redux";
 function App() {
 
   // const [students, setStudents] = useState(data);
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(0)
   const dispatch= useDispatch()
+  const counter = useSelector(state=>state.counter)
+ 
   // dispatch({ type: "GET_STUDENTS "})
   // state is a place that we can hold/store data
   // We store state in functional components using hooks
@@ -16,6 +19,9 @@ function App() {
     <div className="App">
   <h1>App Js</h1>
   <h3>Data starts here</h3>
+  {counter}
+  <input type="number" onChange={(e)=>setInputValue(parseInt(e.target.value))} />
+  <button  onClick={()=>dispatch({type: "THE_COUNTER",payload:inputValue})}>Increase Counter</button>
   <StudentContainer />
     </div>
   );
--- week12/day1/react-review/src/redux/reducers/rootReducer.js
+++ week12/day1/react-review/src/redux/reducers/rootReducer.js
@@ -18,13 +18,17 @@ const initialState = {
         {name: "Xavier"},
         {name: "Jenny"},
         {name: "Pam"}
-    ]
+    ],
+    counter: 0,
 };
 
 function rootReducer(state=initialState, action){
     switch(action.type){
         case "GET_STUDENTS":
             return state;
+        case "THE_COUNTER":
+            return{...state, counter: state.counter + action.payload}
+           
         default:
             return state
     }
