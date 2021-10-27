0af3b8b7fdad0d947358634d680e5863ed2fdf3d
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-27 17:08:47 -0400

    initial navbar style

--- week13/day1/citrus-shop/src/styled-components/Navbar-style.js
+++ week13/day1/citrus-shop/src/styled-components/Navbar-style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const NavBarDiv = styled.div`
     display: flex;
     justify-content: space-around;
-    background-color: pink;
+    background-color: #75E6DA;
+    height: 7vh;
 
 `
