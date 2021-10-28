0bf49501daf4182824737205d4bf8462da386a80
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-28 15:29:18 -0400

    about paragraph written

--- week13/day1/citrus-shop/src/components/About.js
+++ week13/day1/citrus-shop/src/components/About.js
@@ -1,26 +1,43 @@
 import React from 'react';
-
+import { AboutDiv, MainDiv, FormDiv, AboutHeader, AboutParagraph } from '../styled-components/About-style';
 
 
 export default function About() {
     
-    const logo = require('../assets/lemonlime-long.jpeg');
+
     return (
-        <div>
-            <h1>About</h1>
+        <MainDiv>
+           
             <div>
-            <img src="https://images.unsplash.com/photo-1581916679620-dc7b8fb1a30d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGNpdHJ1c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="" />
+            {/* <img src="https://images.unsplash.com/photo-1612483122117-0c6f30110083?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTB8fGNpdHJ1c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" /> */}
    
             <img src="https://images.unsplash.com/photo-1590004829017-f30ef3c5faf1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTJ8fGNpdHJ1c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60" alt="" />
            
             </div>
-            <p>Have you been looking for citrus trees at an affordable price but have come up empty? Are you looking for a specific variety that is even harder to find? Well, you’ve come to the right place! It is our mission to provide the best citrus trees and service to our fellow Floridians at a price that anyone can afford.
+            <AboutDiv>
+            <AboutHeader>About Us</AboutHeader>
+            <AboutParagraph>
+              We are a family owned business established in 2003, located in the heart of 
+              Tampa, Florida. It has been our mission to offer reasonably priced citrus
+               tress to those in our community. Now thanks to the advances in technology and our amazing delivery 
+               drivers we can now offer our quality trees around the country. So no matter which city you live in, 
+               Citrus in the City will be happy to provide you with trees that will grow delicious fruit for you and your 
+               loved ones for years to come.
+            </AboutParagraph>
+            </AboutDiv>
+           
+            <form>
+                <FormDiv>
+                <h2>Contact Us</h2>
+                <input type="text" placeholder="Name"/>
+                <input type="email" placeholder="Email" />
+                <input type="text" placeholder="Subject"/>
+               <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+               <button type="submit">Submit</button>
+               </FormDiv>
+            </form>
 
-            Since 1980, our family has been providing grove owners and home owners with high quality, containerized citrus trees. Located on the Lake Wales Ridge, we are fortunate to be in the very heart of the Florida citrus industry. We have spent more than 50 years and three generations learning the best practices for growing a healthy, vigorous tree and now we can ship them right to your door!</p>
-            <h2>Our Commitment to Quality</h2>
-            <p>We guarantee that each tree we sell is healthy and free of pests or nutrient deficiencies. Your trees will arrive with a dense root ball and will be ready to plant in the ground or a larger pot for containerized growing. Although we guarantee your trees will arrive in great condition, there are many factors that affect the health and production of citrus trees once they leave the greenhouse. Please refer to our Resources page for information regarding planting and overall care of your trees. 
-
-            If you have any questions or concerns upon delivery or during the life of your trees, please send us a message or give us a call at (863) 455-9519 and we will work with you to make it right.</p>
-        </div>
+           
+        </MainDiv>
     )
 }
--- week13/day1/citrus-shop/src/styled-components/About-style.js
+++ week13/day1/citrus-shop/src/styled-components/About-style.js
@@ -1,1 +1,36 @@
 import styled from "styled-components";
+
+export const MainDiv = styled.div`
+    position:absolute;
+    margin-top: 3.59rem;
+    width: 100vw;
+
+    @media (min-width: 600px) and (max-width: 1000px){
+      margin-top: 6rem;
+}
+    @media (min-width: 1001px) {
+        margin-top: 8rem;
+    
+ }
+` 
+
+export const FormDiv = styled.div`
+    background-color: #F49728;
+`
+
+export const AboutDiv = styled.div`
+    border: lightgray 1px solid;
+    margin: 1rem;
+`
+
+export const AboutHeader = styled.h1`
+    color: #0F7C4F;
+    margin: 1rem;
+    text-align: center;
+`
+export const AboutParagraph = styled.p`
+    color: #0F7C4F;
+    margin: 1rem;
+    font-size: 1.3rem;
+    
+`
