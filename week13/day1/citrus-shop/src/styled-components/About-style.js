f7eccee04778a13325689e53530571d2f0973612
Kayla Matthews <kdmatthews77@gmail.com>
2021-10-28 15:55:05 -0400

    about page mobile responsive

--- week13/day1/citrus-shop/src/styled-components/About-style.js
+++ week13/day1/citrus-shop/src/styled-components/About-style.js
@@ -6,10 +6,10 @@ export const MainDiv = styled.div`
     width: 100vw;
 
     @media (min-width: 600px) and (max-width: 1000px){
-      margin-top: 6rem;
+      margin-top: 4.59rem;
 }
     @media (min-width: 1001px) {
-        margin-top: 8rem;
+        margin-top: 6.1rem;
     
  }
 ` 
@@ -31,23 +31,63 @@ export const AboutHeader = styled.h1`
     color: #0F7C4F;
     margin: 1rem;
     text-align: center;
+
+    @media (min-width: 600px) and (max-width: 1000px){
+        font-size:2.7rem;
+}
+    @media (min-width: 1001px) {
+        font-size: 3.5rem;
+    
+ }
 `
 export const AboutParagraph = styled.p`
     color: #0F7C4F;
     margin: 1rem;
     font-size: 1.3rem;
+    @media (min-width: 600px) and (max-width: 1000px){
+        font-size:2rem;
+}
+    @media (min-width: 1001px) {
+        font-size: 2.6rem;
+    
+ }
     
 `
 export const ContactHeader = styled.h1`
     color: #0F7C4F;
     text-align: center;
     margin-top: 0;
+    margin-bottom: 1rem;
+    @media (min-width: 600px) and (max-width: 1000px){
+        font-size:2.7rem;
+}
+    @media (min-width: 1001px) {
+        font-size: 3.5rem;
+    
+ }
 `
 export const FormInput = styled.input`
     margin-bottom: .5rem;
+    height: 1.5rem;
+    @media (min-width: 600px) and (max-width: 1000px){
+        font-size:1.5rem;
+        height: 2rem;
+}
+    @media (min-width: 1001px) {
+        font-size: 2.2rem;
+        height: 3rem;
+    
+ }
 `
 export const FormTextArea = styled.textarea`
     margin-bottom: .5rem;
+    @media (min-width: 600px) and (max-width: 1000px){
+        font-size:1.5rem;
+}
+    @media (min-width: 1001px) {
+        font-size: 2.2rem;
+    
+ }
 `
 export const FormButton = styled.button`
     background-color:#BAC6AD;
@@ -56,6 +96,19 @@ export const FormButton = styled.button`
     color: #0F7C4F;
     height: 2rem;
     width: 14rem;
-    margin-left: 10%;
+    margin-left: 12%;
+    margin-top: .4rem;
     font-size: 1rem;
+
+    @media (min-width: 600px) and (max-width: 1000px){
+        font-size:2rem;
+        height: 2.5rem;
+        width: 33rem;
+}
+    @media (min-width: 1001px) {
+        font-size: 3rem;
+        height: 4rem;
+        width: 45rem;
+    
+ }
 `
