d5f654d3407baf76184d40136764524c88898ea3
Kayla Matthews <kdmatthews77@gmail.com>
2021-11-04 11:34:34 -0400

    update build

--- week13/day1/citrus-shop/src/components/NavBarLinks.js
+++ week13/day1/citrus-shop/src/components/NavBarLinks.js
@@ -5,6 +5,7 @@ import { LinkDiv } from '../styled-components/Sidebar-style';
 export default function NavBarLinks(props) {
     return (
         <LinkDiv>
+        
             <Link to={props.linkData.path}>{props.linkData.name}{<img src={props.linkData.image} alt="" />}</Link>
         </LinkDiv>
     )
